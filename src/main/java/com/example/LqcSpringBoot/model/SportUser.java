abe469a2fc26f05b9363074c993768b6fa03a345
Jasonqingchen <lqc301626@gmail.com>
2022-12-24 20:18:04 +0800

    sport_user reg model and xml creation

---
+++ src/main/java/com/example/LqcSpringBoot/model/SportUser.java
@@ -0,0 +1,57 @@
+package com.example.LqcSpringBoot.model;
+
+import java.io.Serializable;
+
+public class SportUser implements Serializable {
+    private String name;
+
+    private String sex;
+
+    private String phone;
+
+    private String desc;
+
+    private String id;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc == null ? null : desc.trim();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+}
