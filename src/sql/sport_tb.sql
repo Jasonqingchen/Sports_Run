6e5597401a118adcdb5509e6170dc1f51ff5b040
Jasonqingchen <lqc301626@gmail.com>
2023-05-26 15:55:39 +0800

    保存sql脚本

---
+++ src/sql/sport_tb.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 文静的滑降绳
+ Source Server Type    : MySQL
+ Source Server Version : 50562 (5.5.62-log)
+ Source Host           : 1.12.238.58:3306
+ Source Schema         : myadmin
+
+ Target Server Type    : MySQL
+ Target Server Version : 50562 (5.5.62-log)
+ File Encoding         : 65001
+
+ Date: 26/05/2023 15:53:36
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sport_tb
+-- ----------------------------
+DROP TABLE IF EXISTS `sport_tb`;
+CREATE TABLE `sport_tb` (
+  `id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `jjlxr` varchar(255) DEFAULT NULL,
+  `jjlxrdh` varchar(255) DEFAULT NULL,
+  `zb` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `wxm` varchar(255) DEFAULT NULL,
+  `sfzhm` varchar(255) DEFAULT NULL,
+  `gj` varchar(255) DEFAULT NULL,
+  `yfcm` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `zz` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
